bfcc54bfc3ff90115159ce4193276e39658d0261
Bruna Elize <brunaelizer@gmail.com>
2026-06-14 15:50:34 -0300

    Created User entity and excluded options for LGPD

---
+++ ReverseMacros.Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseMacros.Domain.Entities
+{
+    public class User(string name, string email, string passwordHash)
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Name { get; private set; } = string.IsNullOrWhiteSpace(name) ? throw new ArgumentException("Nome obrigatório") : name;
+        public string Email { get; private set; } = email.ToLower().Trim();
+        public string PasswordHash { get; private set; } = passwordHash;
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public bool IsDeleted { get; private set; }
+        public DateTime? DeletedAt { get; private set; }
+
+        public void MarkAsDeleted()
+        {
+            IsDeleted = true;
+            DeletedAt = DateTime.UtcNow;
+        }
+
+        protected User() : this(string.Empty, string.Empty, string.Empty) { }
+    }
+}
